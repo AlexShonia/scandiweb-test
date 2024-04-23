9f5c18a43520e1ca45490c19066a3d556627b515
alex <alex.shonia123@gmail.com>
2024-04-23 18:34:30 +0400

    connect to db, create migration, switch to just react

--- backend/database/migrations/migrate.php
+++ backend/database/migrations/migrate.php
@@ -1,15 +1,38 @@
 <?php
-
 require_once __DIR__ . "/../../vendor/autoload.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(dirname(__DIR__)));
 $dotenv->load();
-
-var_dump($_ENV);
 
 $dbhost = $_ENV['DB_HOST'];
 $dbuser = $_ENV['DB_USER'];
 $port = $_ENV['DB_PORT'];
 $password = $_ENV['DB_PASSWORD'];
 $dbname = $_ENV['DB_NAME'];
+$dsn = "mysql:host=$dbhost";
+
+try {
+    $conn = new PDO($dsn, $dbuser, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "CREATE DATABASE IF NOT EXISTS productsdb";
+    $conn->exec($sql);
+    $sql = "USE productsdb";
+    $conn->exec($sql);
+
+    $sql = "CREATE TABLE IF NOT EXISTS products (
+                sku varchar(255) PRIMARY KEY,
+                name varchar(255) NOT NULL,
+                price float NOT NULL,
+                product_type varchar(255) NOT NULL,
+                product_value varchar(255) NOT NULL
+    )";
+    $conn->exec($sql);
+
+    echo "migration successful";
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My Products</h1>
+        <div>
+          <button>Add</button>
+          <button>Mass Delete</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
